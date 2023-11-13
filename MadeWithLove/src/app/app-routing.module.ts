c49ce7d7fd7d711dde13a82bd508014337c7ee58
Kathitu <al.aracena@duocuc.cl>
2023-11-13 20:27:54 -0300

    agregando paginas

--- MadeWithLove/src/app/app-routing.module.ts
+++ MadeWithLove/src/app/app-routing.module.ts
@@ -11,6 +11,42 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'inicio',
+    loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'publicar',
+    loadChildren: () => import('./publicar/publicar.module').then( m => m.PublicarPageModule)
+  },
+  {
+    path: 'miperfil',
+    loadChildren: () => import('./miperfil/miperfil.module').then( m => m.MiperfilPageModule)
+  },
+  {
+    path: 'explorar',
+    loadChildren: () => import('./explorar/explorar.module').then( m => m.ExplorarPageModule)
+  },
+  {
+    path: 'favoritos',
+    loadChildren: () => import('./favoritos/favoritos.module').then( m => m.FavoritosPageModule)
+  },
+  {
+    path: 'menu',
+    loadChildren: () => import('./menu/menu.module').then( m => m.MenuPageModule)
+  },
+  {
+    path: 'notfound',
+    loadChildren: () => import('./notfound/notfound.module').then( m => m.NotfoundPageModule)
+  },
 ];
 
 @NgModule({
